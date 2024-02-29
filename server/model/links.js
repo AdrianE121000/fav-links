095084b4eda06392303394da7e6688a2aed6e862
AdrianE121000 <paracosa3s@gmail.com>
2024-02-29 09:59:36 -0500

    add links model

---
+++ server/model/links.js
@@ -0,0 +1,77 @@
+import mysql from 'mysql2/promise';
+
+const config = {
+  host: 'localhost',
+  user: 'root',
+  port: 3306,
+  password: 'root',
+  database: 'fav_links_db',
+};
+
+const connection = await mysql.createConnection(config);
+export class LinkModel {
+  static async getLinks({ id }) {
+    const [links] = await connection.query(
+      'SELECT title, url, description, created_at FROM links WHERE user_id = ?;',
+      [id]
+    );
+
+    return links;
+  }
+
+  static async createLink({ input }) {
+    const { title, url, description, user_id } = input;
+
+    try {
+      await connection.query(
+        'INSERT INTO links (title, url, description, user_id) VALUES (?, ?, ?, ?) ;',
+        [title, url, description, user_id]
+      );
+
+      const [link] = await connection.query(
+        'SELECT title, url, description FROM links WHERE id = (SELECT MAX(id) FROM links);'
+      );
+
+      return link[0];
+    } catch (err) {
+      console.log('Se produjo un error!');
+    }
+  }
+
+  static async deleteLink({ id }) {
+    try {
+      const [link] = await connection.query('DELETE FROM links WHERE id = ?;', [
+        id,
+      ]);
+
+      return link.affectedRows === 1;
+    } catch (err) {
+      console.log('Se produjo un error!');
+    }
+  }
+
+  static async updateLink({ id, input }) {
+    const { title, url, description } = input;
+
+    const [link] = await connection.query(
+      'SELECT title, url, description FROM links WHERE id = ?;',
+      [id]
+    );
+
+    try {
+      const [result] = await connection.query(
+        'UPDATE links SET title = ?, url = ?, description = ? WHERE id = ?;',
+        [
+          title === undefined ? link[0].title : title,
+          url === undefined ? link[0].url : url,
+          description === undefined ? link[0].description : description,
+          id,
+        ]
+      );
+
+      return result.affectedRows === 1;
+    } catch (err) {
+      console.log('Se produjo un error');
+    }
+  }
+}
