a43623c416fe03c112015b966b293c12e26988f0
AdrianE121000 <paracosa3s@gmail.com>
2024-02-29 11:49:23 -0500

    add user model

---
+++ server/model/users.js
@@ -0,0 +1,38 @@
+import mysql from 'mysql2/promise';
+import { config } from '../config.js';
+
+const connection = await mysql.createConnection(config);
+
+export class UserModel {
+  static async getUser({ username }) {
+    const [user] = await connection.query(
+      'SELECT * FROM users WHERE username = ?;',
+      [username]
+    );
+
+    return user;
+  }
+
+  static async createUser({ input }) {
+    const { username, password, fullname } = input;
+
+    const [userExist] = await connection.query(
+      'SELECT username, password, fullname FROM users WHERE username = ?;',
+      [username]
+    );
+
+    if (userExist.length !== 0) return { message: 'User already exist' };
+
+    await connection.query(
+      'INSERT INTO users (username, password, fullname) VALUES (?, ?, ?);',
+      [username, password, fullname]
+    );
+
+    const [user] = await connection.query(
+      'SELECT * FROM users WHERE username = ?;',
+      [username]
+    );
+
+    return user;
+  }
+}
