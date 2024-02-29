820217be35c5130492334df92073b4734e66b408
AdrianE121000 <paracosa3s@gmail.com>
2024-02-29 11:49:03 -0500

    add user schema

---
+++ server/schema/users.js
@@ -0,0 +1,15 @@
+import z from 'zod';
+
+const userSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+  fullname: z.string(),
+});
+
+export function validateUser(object) {
+  return userSchema.safeParse(object);
+}
+
+// export function validatePartialUser(object) {
+//   return userSchema.partial().safeParse(object);
+// }
