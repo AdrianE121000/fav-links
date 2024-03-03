8447676e70bb7969a5534e1640241475261dddca
AdrianE121000 <paracosa3s@gmail.com>
2024-03-03 10:56:32 -0500

    update provider

--- client/fav-links/src/context/Provider.jsx
+++ client/fav-links/src/context/Provider.jsx
@@ -1,9 +1,16 @@
+import { parseJwt } from '../lib/utils';
+import { useState } from 'react';
 import AppContext from './Context';
 
 export function AppProvider({ children }) {
-  const tokenJWT = false;
+  const [user, setUser] = useState(localStorage.getItem('user') ?? '');
+  const [logged, setLogged] = useState(
+    parseJwt(localStorage.getItem('token')).exp * 1000 > Date.now()
+  );
 
   return (
-    <AppContext.Provider value={{ tokenJWT }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ logged, setLogged, user, setUser }}>
+      {children}
+    </AppContext.Provider>
   );
 }
