0d24485f24d538945f57e1979166831f6240fad8
AdrianE121000 <paracosa3s@gmail.com>
2024-03-03 10:57:02 -0500

    add private abd public routes

---
+++ client/fav-links/src/routes/PrivateRoutes.jsx
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+import AppContext from '../context/Context';
+import { Navigate } from 'react-router-dom';
+
+export function PrivateRoutes({ children }) {
+  const { logged } = useContext(AppContext);
+
+  return logged ? children : <Navigate to='/signin' />;
+}
