89ed941327d1b5061c373f359e18f3b1cdea12b2
AdrianE121000 <paracosa3s@gmail.com>
2024-03-05 09:28:46 -0500

    add getLinks hook

---
+++ client/fav-links/src/hooks/useGetLinks.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef, useState } from 'react';
+
+export function useGetLinks({ id }) {
+  const [links, setLinks] = useState([]);
+  const [error, setError] = useState(false);
+  const loading = useRef();
+
+  useEffect(() => {
+    loading.current = true;
+    fetch(`http://localhost:3000/links/5`)
+      .then((res) => res.json())
+      .then((json) => {
+        setLinks(json);
+      })
+      .catch(() => setError(true))
+      .finally(() => (loading.current = false));
+  }, [id]);
+
+  return { links, error, loading };
+}
