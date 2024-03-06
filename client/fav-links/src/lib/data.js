d8fbd75f2088220cf766b0f890f2d053232b2dd8
AdrianE121000 <paracosa3s@gmail.com>
2024-03-06 11:23:23 -0500

    add createUser and deleteLink method

--- client/fav-links/src/lib/data.js
+++ client/fav-links/src/lib/data.js
@@ -12,6 +12,37 @@ export async function loginUser(username, data) {
 
     return result;
   } catch (err) {
+    console.log('e');
+  }
+}
+
+export async function createUser(data) {
+  try {
+    const res = await fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await res.json();
+
+    return result;
+  } catch (error) {
+    console.log('e');
+  }
+}
+
+export async function deleteLink({ id }) {
+  try {
+    const res = await fetch(`http://localhost:3000/links/${id}`, {
+      method: 'DELETE',
+    });
+    const result = await res.json();
+
+    return result;
+  } catch (error) {
     console.log('error');
   }
 }
