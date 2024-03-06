cc7722e547a265ab2006b6e5f169e204fec616f5
AdrianE121000 <paracosa3s@gmail.com>
2024-03-06 11:34:33 -0500

    update provider

--- client/fav-links/src/context/Provider.jsx
+++ client/fav-links/src/context/Provider.jsx
@@ -3,13 +3,12 @@ import { useState } from 'react';
 import AppContext from './Context';
 
 export function AppProvider({ children }) {
-  const [user, setUser] = useState(localStorage.getItem('user') ?? '');
   const [logged, setLogged] = useState(
     parseJwt(localStorage.getItem('token')).exp * 1000 > Date.now()
   );
 
   return (
-    <AppContext.Provider value={{ logged, setLogged, user, setUser }}>
+    <AppContext.Provider value={{ logged, setLogged }}>
       {children}
     </AppContext.Provider>
   );
