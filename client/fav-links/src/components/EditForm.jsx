202987a15603af833023acf15f043e756a817a92
AdrianE121000 <paracosa3s@gmail.com>
2024-03-08 11:00:20 -0500

    add component EditForm

---
+++ client/fav-links/src/components/EditForm.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import { updateLink } from '../lib/data';
+
+function EditForm({ id }) {
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [description, setDescription] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      url,
+      description,
+    };
+
+    const result = await updateLink({ data, id });
+
+    console.log(result);
+  }
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+        <div className='mb-4'>
+          <label
+            className='block text-gray-700 text-sm font-bold mb-2'
+            htmlFor='title'>
+            Title:
+          </label>
+          <input
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            id='title'
+            type='text'
+            required
+            placeholder='Title'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className='mb-4'>
+          <label
+            className='block text-gray-700 text-sm font-bold mb-2'
+            htmlFor='url'>
+            URL:
+          </label>
+          <input
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            id='url'
+            type='url'
+            required
+            placeholder='URL'
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+        </div>
+        <div className='mb-6'>
+          <label
+            className='block text-gray-700 text-sm font-bold mb-2'
+            htmlFor='description'>
+            Description:
+          </label>
+          <textarea
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            id='description'
+            placeholder='Description'
+            required
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className='flex items-center justify-between'>
+          <button
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+            type='submit'>
+            Aceptar
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default EditForm;
