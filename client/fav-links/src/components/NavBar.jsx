f3a179f1291ab5d946434732d2eefff50f81c2ec
AdrianE121000 <paracosa3s@gmail.com>
2024-03-09 14:33:31 -0500

    add improved navbar

--- client/fav-links/src/components/NavBar.jsx
+++ client/fav-links/src/components/NavBar.jsx
@@ -3,9 +3,22 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <>
-      <nav>
-        <NavLink to='/signin'>Login</NavLink>
-        <NavLink to='/signup'>Sign Up</NavLink>
+      <nav className='bg-gray-800 text-white font-bold text-lg p-4 flex items-center justify-between'>
+        <div className='md:ml-10 hover:text-gray-300'>
+          <NavLink to='/'>Fav Links</NavLink>
+        </div>
+        <div className='flex gap-2'>
+          <NavLink
+            className='hover:text-gray-300'
+            to='/signin'>
+            Login
+          </NavLink>
+          <NavLink
+            className='hover:text-gray-300'
+            to='/signup'>
+            Sign Up
+          </NavLink>
+        </div>
       </nav>
     </>
   );
