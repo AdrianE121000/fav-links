baf88b278df4448cf0ce4f1d417d9a089fde1ca7
AdrianE121000 <paracosa3s@gmail.com>
2024-03-10 11:09:31 -0400

    fix fetch links

--- client/fav-links/src/components/Home.jsx
+++ client/fav-links/src/components/Home.jsx
@@ -3,17 +3,9 @@ import { Card } from './Card';
 
 export function Home() {
   const userId = localStorage.getItem('userID');
-  const { links, loading, error } = useGetLinks(userId);
+  const { links, error } = useGetLinks(userId);
 
   return (
-    <div>
-      {loading.current ? (
-        <h1>cargando</h1>
-      ) : error ? (
-        <h1>se produjo en error</h1>
-      ) : (
-        <Card links={links} />
-      )}
-    </div>
+    <div>{error ? <h1>se produjo en error</h1> : <Card links={links} />}</div>
   );
 }
--- client/fav-links/src/hooks/useGetLinks.js
+++ client/fav-links/src/hooks/useGetLinks.js
@@ -1,20 +1,17 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function useGetLinks(id) {
   const [links, setLinks] = useState([]);
   const [error, setError] = useState(false);
-  const loading = useRef();
 
   useEffect(() => {
-    loading.current = true;
     fetch(`http://localhost:3000/links/${id}`)
       .then((res) => res.json())
       .then((json) => {
         setLinks(json);
       })
-      .catch(() => setError(true))
-      .finally(() => (loading.current = false));
+      .catch(() => setError(true));
   }, [id]);
 
-  return { links, error, loading };
+  return { links, error };
 }
