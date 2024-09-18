98103cfd0102d9e49537f2f0b8343bbb0efc59a0
AdrianE121000 <paracosa3s@gmail.com>
2024-09-18 12:07:54 -0500

    add group name to the links on cards

--- client/fav-links/src/components/Card.jsx
+++ client/fav-links/src/components/Card.jsx
@@ -1,44 +1,78 @@
+import { useEffect, useState } from "react";
 import { timeAgo } from "../lib/utils";
 import { DeleteIcon, EditIcon } from "./Icons";
 
-export function Card({ userLinks, onDelete, onEdit }) {
-  const sortedLinks = userLinks.sort((a, b) => b.id - a.id);
+export function Card({ link, onDelete, onEdit }) {
+  const [groupName, setGroupName] = useState("");
+
+  useEffect(() => {
+    const getGroupName = async () => {
+      const res = await fetch(
+        `http://localhost:3000/groups/goupname/${link.category_id}`
+      );
+
+      if (!res.ok) {
+        return;
+      } else {
+        const result = await res.json();
+
+        setGroupName(result[0].name);
+      }
+    };
+
+    getGroupName();
+  }, [link.category_id]);
 
   return (
     <>
-      <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 mt-2">
-        {sortedLinks.map((link) => (
-          <div
-            key={link.id}
-            className="bg-neutral-800 flex flex-col h-[180px] justify-between text-white rounded-lg p-2 m-3 shadow-sm shadow-white"
+      <div
+        key={link.id}
+        className="bg-neutral-800 flex flex-col h-[180px] justify-between text-white rounded-lg p-2 m-3 shadow-sm shadow-white"
+      >
+        <a
+          href={link.url}
+          target="_blank"
+          className="text-lg font-bold hover:text-blue-700 line-clamp-3 duration-500 ease-in-out"
+        >
+          {link.title}
+        </a>
+        <p className="text-gray-300 line-clamp-2">{link.description}</p>
+        <p>{timeAgo(link.created_at)}</p>
+        {groupName ? (
+          <p>
+            group:{"  "}
+            <span>
+              {" "}
+              {groupName}{" "}
+              {/* <button className="items-center font-semibold rounded bg-red-500 text-white text-xs px-2 py-1 hover:bg-red-900 hover:scale-110 transition duration-500 ease-in-out hover:shadow-lg hover:shadow-red-700">
+                Remove
+              </button> */}
+            </span>
+          </p>
+        ) : (
+          <span>
+            {/* <button className="items-center font-semibold rounded bg-blue-500 text-white text-sm px-2 py-1 hover:bg-blue-900 hover:scale-110 transition duration-500 ease-in-out hover:shadow-lg hover:shadow-blue-700">
+              Add to a group
+            </button> */}
+          </span>
+        )}
+
+        <div className="mt-4 flex justify-end items-center gap-1">
+          <button
+            onClick={() => onEdit(link.id)}
+            className="flex gap-2 items-center font-semibold rounded bg-blue-500 text-white text-sm px-2 py-1 hover:bg-blue-900 hover:scale-110 transition duration-500 ease-in-out hover:shadow-lg hover:shadow-blue-700"
           >
-            <a
-              href={link.url}
-              target="_blank"
-              className="text-lg font-bold hover:text-blue-700 line-clamp-3 duration-500 ease-in-out"
-            >
-              {link.title}
-            </a>
-            <p className="text-gray-300 line-clamp-2">{link.description}</p>
-            <p>{timeAgo(link.created_at)}</p>
-            <div className="mt-4 flex justify-end items-center gap-1">
-              <button
-                onClick={() => onEdit(link.id)}
-                className="flex gap-2 items-center font-semibold rounded bg-blue-500 text-white text-sm px-2 py-1 hover:bg-blue-900 hover:scale-110 transition duration-500 ease-in-out hover:shadow-lg hover:shadow-blue-700"
-              >
-                Edit
-                <EditIcon />
-              </button>
-              <button
-                onClick={() => onDelete(link.id)}
-                className="flex gap-2 items-center font-semibold rounded bg-red-500 text-white text-sm px-2 py-1 hover:bg-red-900 hover:scale-110 transition duration-500 ease-in-out hover:shadow-lg hover:shadow-red-700"
-              >
-                Delete
-                <DeleteIcon />
-              </button>
-            </div>
-          </div>
-        ))}
+            Edit
+            <EditIcon />
+          </button>
+          <button
+            onClick={() => onDelete(link.id)}
+            className="flex gap-2 items-center font-semibold rounded bg-red-500 text-white text-sm px-2 py-1 hover:bg-red-900 hover:scale-110 transition duration-500 ease-in-out hover:shadow-lg hover:shadow-red-700"
+          >
+            Delete
+            <DeleteIcon />
+          </button>
+        </div>
       </div>
     </>
   );
